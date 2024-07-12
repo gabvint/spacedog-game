00126499799601e9329792bb0f12be88ec11b044
gabtongol <geybtongol@gmail.com>
2024-07-12 19:47:21 -0400

    keyboard

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 “SpaceDog”, inspired by the classic “Spaceman” game. The mechanics will remain the same as the inspired game but spacedog will introduce a theme revolving around aliens and a dog. 
 In Spacedog, players must prevent a dog from being abducted by aliens. The objective is to correctly guess the word by selecting letters. Each incorrect guess brings the dog closer to abduction
 
+## SCREENSHOTS 
+
+## TECHNOLOGIES USED 
+
+## GETTING STARTED 
+
--- js/app.js
+++ js/app.js
@@ -1,14 +1,84 @@
 /*-------------- Constants -------------*/
-
+const warning_message = 'Arf, 1 chance left'
+const win_message = "Hey, you won!"
+const lost_message = "Awww"
+const max_chances = 5
 
 /*---------- Variables (state) ---------*/
+
+let hiddenWord
+let guessedWord
+let hint
+let playerChoice 
+let playerChances 
+let playerLost
+
+
 
 
 /*----- Cached Element References  -----*/
 
+const letter = document.querySelectorAll('.key')
+const keyboard = document.querySelectorAll('#keyboard')
+
+
 
 /*-------------- Functions -------------*/
 
+const init = () => {
+    hiddenWord = getHiddenWord(); 
+    hint = getHint();   
+    playerChances = max_chances
+    playerLost = false
+}
+
+async function getHiddenWord(){
+    let data;
+    let wordArray;
+    let hiddenWord
+    const fs = require('fs').promises
+
+    try {
+        data = await fs.readFile('wordlist.txt', 'utf-8')
+        wordArray = data.split('\n')
+        console.log(wordArray.length)
+    } catch (err){
+        console.log("error getting file")
+    }
+
+
+}
+
+const getHint = () => {
+
+}
 
 /*----------- Event Listeners ----------*/
+
+getHiddenWord()
+
+
+
+// console.dir(keyboard)
+// console.dir(letter)
+
+// letter.forEach(key => {
+//     key.addEventListener('click', (event) =>{
+//         console.log(event.target.id)
+//     })
+// })
+
+// const fs = require('fs').promises;
+
+// async function readFileAndConvertToArray() {
+//     try {
+//         let data = await fs.readFile('wordlist.txt', 'utf8');
+//         const wordsArray = data.split('\n');
+//         console.log(wordsArray[2]); // Output: [ 'apple', 'banana', 'cherry', 'date', 'elderberry' ]
+//     } catch (err) {
+//         console.log("error");
+//     }
+// }
+
+
 
