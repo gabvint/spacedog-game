3f16e456778a7f059d4e9b85f70ca156903656c4
gabtongol <geybtongol@gmail.com>
2024-07-12 22:49:03 -0400

    updates

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
 
-const { read } = require('fs')
-
 /*-------------- Constants -------------*/
 const warning_message = 'Arf, 1 chance left'
 const win_message = "Hey, you won!"
@@ -20,10 +18,11 @@ let playerLost
 /*----- Cached Element References  -----*/
 
 
-const keys = document.querySelectorAll('.key')
-const keyboard = document.querySelectorAll('#keyboard')
+const keysEls = document.querySelectorAll('.key')
+const keyboardEls = document.querySelectorAll('#keyboard')
+const wordEls = document.querySelector('.word')
 
-keys.forEach(key => {
+keysEls.forEach(key => {
     key.addEventListener('click', (event) =>{
         console.log(event.target.id)
     })
@@ -34,42 +33,32 @@ keys.forEach(key => {
 
 async function init (){
 
-
     let stringFromFile = await getArrayOfWords() 
     // splits the string into 2 and removes |
     let parts = stringFromFile.split('|')
 
-    // assigns first part of string wc is the word and removes the whitespace
+    // assigns word and hint and removes the whitespace
     hiddenWord = parts[0].trim()
-    console.log(hiddenWord)
-     // assigns second part of string wc is the hint and removes the whitespace
     hint = parts[1].trim()
-    console.log(hint) 
 
     playerChances = max_chances
     playerLost = false
+
 }
 
 async function getArrayOfWords() {
-    //gets a random string from the file wordlist.txt 
-    // format : word | hint:...
-    const fs = require('fs').promises; 
-    let index; 
     try {
-
-        let data = await fs.readFile('wordlist.txt', 'utf8');
-
-        const wordsArray = data.split('\n') //makes it to an array
-        index = Math.floor( Math.random() * wordsArray.length)
-
-        return wordsArray[index]
-
+        let response = await fetch('wordlist.txt');
+        let data = await response.text();
+        const wordsArray = data.split('\n');
+        let index = Math.floor(Math.random() * wordsArray.length);
+        return wordsArray[index];
     } catch (err) {
         console.log("error");
     }
-
 }
 
+ 
 
 
 
