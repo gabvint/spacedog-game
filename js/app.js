c4edb775e20fd177a8601a8ef9f0a1b1db340f42
gabtongol <geybtongol@gmail.com>
2024-07-12 21:03:13 -0400

    file handling words

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+const { Console } = require('console')
+const { read } = require('fs')
+
 /*-------------- Constants -------------*/
 const warning_message = 'Arf, 1 chance left'
 const win_message = "Hey, you won!"
@@ -18,44 +21,63 @@ let playerLost
 
 /*----- Cached Element References  -----*/
 
-const letter = document.querySelectorAll('.key')
-const keyboard = document.querySelectorAll('#keyboard')
+
+// const letter = document.querySelectorAll('.key')
+// const keyboard = document.querySelectorAll('#keyboard')
 
 
 
-/*-------------- Functions -------------*/
+// /*-------------- Functions -------------*/
 
-const init = () => {
-    hiddenWord = getHiddenWord(); 
-    hint = getHint();   
+async function init (){
+
+    hiddenWord = await getHiddenWords(); 
+    console.log(hiddenWord)
+   // hint = getHint();   
     playerChances = max_chances
     playerLost = false
 }
 
-async function getHiddenWord(){
-    let data;
-    let wordArray;
-    let hiddenWord
-    const fs = require('fs').promises
+async function getArrayOfWords() {
+
+    const fs = require('fs').promises; 
+    let index; 
 
     try {
-        data = await fs.readFile('wordlist.txt', 'utf-8')
-        wordArray = data.split('\n')
-        console.log(wordArray.length)
-    } catch (err){
-        console.log("error getting file")
+
+        let data = await fs.readFile('wordlist.txt', 'utf8');
+
+        const wordsArray = data.split('\n') //makes it to an array
+        
+        //index = Math.floor( Math.random() * wordsArray.length / 2 ) * 2;
+
+        return wordsArray
+
+    } catch (err) {
+        console.log("error");
     }
 
+}
+
+async function getHiddenWords(){
+
+    let wordArray = await getArrayOfWords()
+    let index = Math.floor( Math.random() * wordArray.length / 2 ) * 2
+
+    return wordArray[index]
 
 }
 
-const getHint = () => {
-
+async function getHint(){
+    
 }
+// const getHint = () => {
+
+// }
 
 /*----------- Event Listeners ----------*/
 
-getHiddenWord()
+//init()
 
 
 
@@ -67,18 +89,6 @@ getHiddenWord()
 //         console.log(event.target.id)
 //     })
 // })
-
-// const fs = require('fs').promises;
-
-// async function readFileAndConvertToArray() {
-//     try {
-//         let data = await fs.readFile('wordlist.txt', 'utf8');
-//         const wordsArray = data.split('\n');
-//         console.log(wordsArray[2]); // Output: [ 'apple', 'banana', 'cherry', 'date', 'elderberry' ]
-//     } catch (err) {
-//         console.log("error");
-//     }
-// }
 
 
 
